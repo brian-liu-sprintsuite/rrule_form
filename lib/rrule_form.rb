745c255a80ca3994eeee4db4d9778f63e39f0653
Brian Liu <brianlau195@gmail.com>
2025-10-24 16:51:42 +1030

    patch for rails 7

--- lib/rrule_form.rb
+++ lib/rrule_form.rb
@@ -3,15 +3,11 @@ require "rrule_form/version"
 module RruleForm
   class Error < StandardError; end
 
-  module Rails
-    class Engine < ::Rails::Engine
+  class Engine < ::Rails::Engine
+    isolate_namespace RruleForm
 
-      isolate_namespace RruleForm
-      initializer 'local_helper.action_controller' do
-        ActiveSupport.on_load :action_controller do
-          helper RruleForm::ApplicationHelper
-        end
-      end
+    ActiveSupport.on_load(:action_controller_base) do
+      helper RruleForm::ApplicationHelper
     end
   end
 end
